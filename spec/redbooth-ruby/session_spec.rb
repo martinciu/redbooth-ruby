61be43120cec52060fc86f54aa047c5d9c84d047
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-02-03 10:10:16 +0100

    mock refresh_token request

--- spec/redbooth-ruby/session_spec.rb
+++ spec/redbooth-ruby/session_spec.rb
@@ -2,33 +2,26 @@ require "spec_helper"
 
 describe RedboothRuby::Session do
   include_context 'authentication'
-  let(:token) { access_token[:token] }
-  let(:refresh_token) { access_token[:refresh_token] }
 
   describe 'initialize' do
     subject { described_class.new(access_token) }
 
-    it { expect(subject.refresh_token).to eq refresh_token }
+    it { expect(subject.refresh_token).to eq access_token[:refresh_token] }
   end
 
-  describe '#refresh' do
+  describe '#refresh', vcr: 'refresh_token' do
     subject { described_class.new(access_token).refresh! }
 
     context 'refresh token present' do
-      let(:oauth_client) { double(:oauth_client) }
-      let(:oauth_access_token) { double(:access_token, refresh!: new_oauth_access_token) }
-      let(:new_oauth_access_token) { double(:access_token, token: 'new_token', refresh_token: 'new_refresh_token' ) }
+      it { expect(subject.refresh_token).to eq 'new_refresh_token_token' }
+    end
 
+    context 'refresh token not present' do
       before do
-        allow(OAuth2::Client).to receive(:new).with(consumer_key, consumer_secret, described_class::OAUTH_URLS).and_return(oauth_client)
-        allow(OAuth2::AccessToken).to receive(:new).with(oauth_client, token, { refresh_token: refresh_token }).and_return(oauth_access_token)
+        access_token.delete(:refresh_token)
       end
 
-      it { expect(subject).to eq new_oauth_access_token }
-      it do
-        expect(oauth_access_token).to receive(:refresh!)
-        subject
-      end
+      it { expect { subject }.to raise_exception }
     end
   end
 end
